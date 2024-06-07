f791a1a77e57ec091ce352392d9c18709252ec53
rahatfaruk <rahatfaruk99@gmail.com>
2024-06-07 16:35:09 +0600

    Payment-route added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
+const stripe = require('stripe')(process.env.SecretPaymentKey)
 
 const app = express()
 const port = 3000
@@ -11,6 +12,20 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Welcome')
+})
+
+// stripe element payment
+app.post('/create-payment-intent', async (req, res) => {
+  const {price} = req.body 
+  // flatten price
+  const flatPrice = parseInt(price * 100)
+  
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: flatPrice,
+    currency: "usd"
+  })
+
+  res.send({clientSecret: paymentIntent.client_secret})
 })
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`))
