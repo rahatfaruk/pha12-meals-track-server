0793d542e8b76f89e9528edcf842f4024ce929bf
rahatfaruk <rahatfaruk99@gmail.com>
2024-06-10 21:06:22 +0600

    setup Nd connect mondodb

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const stripe = require('stripe')(process.env.SecretPaymentKey)
 
+const mongoUri = `mongodb+srv://${process.env.UserMDB}:${process.env.PasswordMDB}@cluster0.ympa4ek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const client = new MongoClient(mongoUri, {
+  serverApi: { version: ServerApiVersion.v1, strict:true, deprecationErrors:true }
+})
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Welcome')
-})
+async function main() {
+  try {
+    await client.connect()
+    const database = client.db('pha12')
+    // collections
+    const collUsers = database.collection('users')
 
-// stripe element payment
-app.post('/create-payment-intent', async (req, res) => {
-  const {price} = req.body 
-  // flatten price
-  const flatPrice = parseInt(price * 100)
-  
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: flatPrice,
-    currency: "usd"
-  })
 
-  res.send({clientSecret: paymentIntent.client_secret})
-})
+    app.get('/', (req, res) => {
+      res.send('Welcome')
+    })
+    
+    // stripe element payment
+    app.post('/create-payment-intent', async (req, res) => {
+      const {price} = req.body 
+      // flatten price
+      const flatPrice = parseInt(price * 100)
+      
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: flatPrice,
+        currency: "usd"
+      })
+    
+      res.send({clientSecret: paymentIntent.client_secret})
+    })
+
+    // check connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment")
+    } catch (error) {
+      console.log("Error in deployment", error?.message)
+  }
+}
+
+main()
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`))
 
